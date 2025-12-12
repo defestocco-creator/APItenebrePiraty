d5229f6ef8498574f11b053a16d02b3f98da0873
defestocco-creator <defesto.cco@gmail.com>
2025-12-12 04:45:27 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import axios from "axios";
-import cheerio from "cheerio";
+import * as cheerio from "cheerio";
 import dotenv from "dotenv";
 import admin from "firebase-admin";
 
